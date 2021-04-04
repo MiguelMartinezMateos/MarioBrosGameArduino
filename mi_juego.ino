43d2e26e68ce573572b40f772c569b755d95a7bd
MiguelMartinezMateos <47322591+MiguelMartinezMateos@users.noreply.github.com>
2021-04-04 19:23:20 +0200

    sounds, home and game over screens

--- mi_juego.ino
+++ mi_juego.ino
@@ -1,58 +1,75 @@
 /*
- * 
- * PROYECO DESARROLLADO POR MIGUEL MARTINEZ MATEOS
- * 
- */
 
+   PROYECO DESARROLLADO POR MIGUEL MARTINEZ MATEOS
 
-
-
+*/
 #include <Adafruit_SSD1306.h>
 #include <Fonts/FreeSans9pt7b.h>
 Adafruit_SSD1306 display(128, 64, &Wire, 4);
 
+
+#define crashsound1 1100
+#define crashsound2 1500
+#define jumpsound 1200
+#define bendsound 1500
+
+
 //enemies
 int enemie1_x = 130; //el recorrido es de 130 a -30 es decir 160 entonces la otra deberia ir a 160+128 para ir equidistantes pero si lo altero un poco es más dificil y queda menos medido y mas natural
 int enemie2_x = 240;
-uint8_t duracionsalto = 11;
-int8_t vel_enem = -5;
-unsigned long tiempo_actual = 0;
-unsigned long tiempo_aux = 0;
+uint8_t jumpduration = 11;
+int8_t enem_vel = -5;
+unsigned long current_time = 0;
+unsigned long aux_time = 0;
 boolean lock = 0;
 boolean lock2 = 0;
 boolean locklives = 0;
 boolean lockscore = 0;
 unsigned int rando = 0;
 unsigned int rando2 = 0;
-int8_t estado = 0; //0 para normal, 1 para saltando y 2 para agachado
-boolean muerto = 0;
+int8_t state = 0; //0 para normal, 1 para saltando y 2 para agachado
+boolean dead = 0;
 uint8_t lives = 1;
 int score = 0;
 
 //sounds
-const int c = 261;
-const int d = 294;
-const int e = 329;
-const int f = 349;
-const int g = 391;
-const int gS = 415;
-const int a = 440;
-const int aS = 455;
-const int b = 466;
-const int cH = 523;
-const int cSH = 554;
-const int dH = 587;
-const int dSH = 622;
-const int eH = 659;
-const int fH = 698;
-const int fSH = 740;
-const int gH = 784;
-const int gSH = 830;
-const int aH = 880;
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+
+#define melodyPin 9
 
 
+//sounds
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7, 0, NOTE_C7, NOTE_E7, 0, NOTE_G7, 0, 0,  0, NOTE_G6, 0, 0, 0, NOTE_C7, 0, 0, NOTE_G6, 0, 0, NOTE_E6, 0, 0, NOTE_A6, 0, NOTE_B6, 0, NOTE_AS6, NOTE_A6, 0, NOTE_G6, NOTE_E7, NOTE_G7, NOTE_A7, 0, NOTE_F7, NOTE_G7, 0, NOTE_E7, 0, NOTE_C7, NOTE_D7, NOTE_B6, 0, 0
+};
+int tempo[] = {
+  12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 9, 9, 9, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12
+};
 
-//mario
+//graphics
 const unsigned char PROGMEM mario[] = {
   0x1, 0xE0, 0x6, 0x10, 0x8, 0x10, 0x10, 0xDC, 0x23, 0xFE, 0x26, 0x2C, 0x7C, 0x58, 0x4C, 0x5C, 0x4C, 0x2, 0x4E, 0x2, 0x44, 0x82, 0x21, 0xC7, 0x18, 0x5C, 0x3C, 0x30, 0x47, 0xC0, 0x47, 0x34, 0x88, 0xBA, 0x90, 0x4A, 0x90, 0x4C, 0xC8, 0x94, 0x7F, 0x34, 0x7F, 0xFE, 0x3F, 0xFD, 0x5F, 0xF9, 0x5F, 0x71, 0x4E, 0x11, 0x21, 0x12, 0x1F, 0xC
 };
@@ -77,28 +94,51 @@ const unsigned char PROGMEM cloud[] = {
 const unsigned char PROGMEM mushroom[] = {
   0x7, 0xF0, 0x0, 0x1F, 0xCC, 0x0, 0x27, 0xC2, 0x0, 0x4F, 0xE1, 0x0, 0x58, 0x31, 0x0, 0xF0, 0x1F, 0x80, 0xF0, 0x19, 0x80, 0xB0, 0x10, 0x80, 0x98, 0x30, 0x80, 0x9F, 0xF9, 0x80, 0xBF, 0xFF, 0x80, 0x72, 0x27, 0x0, 0x22, 0x22, 0x0, 0x20, 0x2, 0x0, 0x10, 0x4, 0x0, 0xF, 0xF8, 0x0,
 };
+const unsigned char PROGMEM mariowallpaper[] = {  //size 64X64
+  0x0, 0x0, 0x0, 0x0, 0x40, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x80, 0x0, 0x80, 0x0, 0x0, 0x0, 0x0, 0x3F, 0x88, 0x0, 0x80, 0x0, 0x7, 0x0, 0x1, 0xFF, 0x19, 0x80, 0x40, 0x0, 0x8, 0x80, 0xF, 0xFE, 0x2A, 0x80, 0x40, 0x0, 0x8, 0x40, 0x3F, 0xFC, 0x2A, 0x80, 0x20, 0x0, 0x8, 0x40, 0x7F, 0xFC, 0x44, 0x80, 0x20, 0x0, 0x8, 0x20, 0xFF, 0xFC, 0x41, 0x0, 0x30, 0x0, 0x4, 0x21, 0xFE, 0x3, 0x1, 0x0, 0x18, 0x0, 0x4, 0x13, 0xF8, 0x0, 0xC2, 0x0, 0x1C, 0x0, 0x2, 0x1F, 0xF0, 0x10, 0x30, 0x0, 0x1E, 0x0, 0x2, 0xF, 0xF0, 0x8, 0x8, 0x0, 0xF, 0x0, 0x1, 0xF, 0xE0, 0x6, 0x4, 0x0, 0xF, 0x0, 0x1, 0x7, 0xE0, 0x1, 0x82, 0x0, 0xF, 0x80, 0x6, 0x87, 0xE0, 0x0, 0x41, 0x0, 0xF, 0x80, 0x8, 0x83, 0xE0, 0x0, 0x20, 0x80, 0xF, 0xC0, 0x3B, 0xE3, 0xF0, 0xF8, 0x1E, 0x40, 0x7, 0xC0, 0x4C, 0x11, 0xF1, 0xFF, 0xFF, 0x20, 0x7, 0xE0, 0x48, 0x9, 0xF9, 0x84, 0x65, 0xA0, 0x7, 0xE0, 0x48, 0x0, 0xFD, 0x0, 0x42, 0x90, 0x7, 0xF0, 0x28, 0x0, 0xFF, 0x30, 0x1, 0x10, 0x7, 0xF0, 0x64, 0x0, 0xFF, 0x48, 0x31, 0x8, 0x7, 0xF0, 0x93, 0xC0, 0xFE, 0x84, 0x48, 0x88, 0x7, 0xF8, 0x89, 0x20, 0xFE, 0x9C, 0x84, 0x84, 0x7, 0xF8, 0x86, 0x21, 0x3E, 0x9C, 0x84, 0x44, 0x3, 0xF8, 0x41, 0x10, 0x3F, 0x9C, 0x84, 0x3E, 0x3, 0xFC, 0x32, 0x0, 0x78, 0x68, 0x9C, 0x2, 0x2, 0x7C, 0x2C, 0x0, 0xA0, 0x10, 0x9C, 0x1, 0x2, 0x7C, 0x20, 0x1, 0x20, 0x8, 0x58, 0x3, 0x2, 0x3E, 0x10, 0x1, 0x40, 0x0, 0x30, 0x7, 0x80, 0x3E, 0x10, 0x2, 0x40, 0x0, 0x0, 0x3F, 0x80, 0x3E, 0x38, 0xC, 0x40, 0x8, 0xE, 0x7F, 0xC0, 0x3E, 0x38, 0x30, 0xE0, 0x8, 0xFE, 0x7F, 0xDC, 0x3E, 0x3F, 0xC0, 0xE0, 0x1F, 0xFC, 0x3F, 0xE2, 0x3F, 0x3F, 0x80, 0x78, 0x7F, 0xF0, 0x7F, 0x81, 0x7F, 0x3F, 0x80, 0x7F, 0xFF, 0xE0, 0xFE, 0x1, 0xFF, 0x3F, 0xC0, 0x5F, 0xFF, 0x80, 0xFC, 0x19, 0xFF, 0x1F, 0xC0, 0x4D, 0xB0, 0x0, 0x70, 0x21, 0xFF, 0x1F, 0xE0, 0x40, 0x1, 0xE0, 0x0, 0x51, 0xFF, 0x1F, 0xE0, 0x23, 0xFE, 0x20, 0x0, 0x63, 0xFF, 0x1F, 0xF0, 0x10, 0x80, 0x20, 0x0, 0x43, 0xFF, 0x1F, 0xF0, 0x10, 0xFF, 0xE0, 0x0, 0x27, 0xFF, 0x1F, 0xF8, 0x8, 0xFF, 0xC0, 0x2, 0x7, 0xFF, 0x1F, 0xF8, 0x8, 0xC0, 0x80, 0x7, 0xF, 0xFE, 0xF, 0xFC, 0x8, 0x40, 0x80, 0xF, 0xFF, 0xFE, 0xF, 0xFE, 0x4, 0x7F, 0x0, 0xFF, 0xFF, 0xFE, 0xF, 0xFF, 0x4, 0x0, 0x3, 0x1F, 0xFF, 0xFE, 0x7, 0xFF, 0x8B, 0x0, 0xC, 0x27, 0xFF, 0xFE, 0x7, 0xFF, 0xD0, 0x80, 0x70, 0xC1, 0xFF, 0xFC, 0x7, 0xFF, 0xE1, 0x7F, 0x81, 0x0, 0x7F, 0xFC, 0x3, 0xFF, 0xE2, 0x0, 0x81, 0x0, 0x1F, 0xF8, 0x3, 0xFF, 0xCC, 0x1, 0x2, 0x0, 0xF, 0xF8, 0x1, 0xFF, 0xC8, 0x7E, 0x3, 0x80, 0x7, 0xF0, 0x1, 0xFF, 0x88, 0x87, 0xC6, 0x40, 0x3, 0xF0, 0x0, 0xFF, 0x91, 0x0, 0x3C, 0x7C, 0x3, 0xE0, 0x0, 0xFF, 0x16, 0x0, 0x44, 0x43, 0x1, 0xC0, 0x0, 0x7F, 0x98, 0x0, 0x22, 0x20, 0x81, 0x80, 0x0, 0x7E, 0x5B, 0x0, 0x0, 0x20, 0x0, 0x80, 0x0, 0x3C, 0x3C, 0x3, 0x80, 0x20, 0x0, 0x80, 0x0, 0x3C, 0x38, 0x0, 0x0, 0x20, 0x0, 0x80, 0x0, 0x1C, 0x30, 0x0, 0x0, 0x20, 0x0, 0x80, 0x0, 0xC, 0x31, 0x3, 0x80, 0x20, 0x0, 0x80, 0x0, 0x2, 0x51, 0x0, 0x2, 0x40, 0x1, 0x0, 0x0, 0x1, 0x88, 0x80, 0x2, 0x40, 0x2, 0x0,
+};
+
 
 
 void setup() {
-
   pinMode(3, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
   display.clearDisplay();
-  display.setTextSize(2);
+  display.drawBitmap(0, 0, mariowallpaper, 64, 64, 1);
+  display.setTextSize(0);
   display.setFont();
   display.setTextColor(WHITE);
-  display.setCursor(0, 20);
-  display.println("SUPERMARIO");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-  delay(500);
-  Serial.begin(115200);
+  display.setCursor(70, 0);
+  display.println("ARDUGAME");
+  display.setFont(&FreeSans9pt7b);
+
+  display.setCursor(64, 20);
+  display.println("MARIO");
   display.display();
 
+  display.setFont();
+  display.setCursor(85, 23);
+  display.println("by");
+  display.display();
+
+  display.setFont();
+  display.setCursor(70, 33);
+  display.println("MMMateos");
+  display.display();
+
+  display.setCursor(70, 48);
+  display.println("A = Jump");
+  display.setCursor(70, 57);
+  display.println("B = Bend");
+
+  display.display();
+
+  sing(0);
+  delay(1000);
 }
 
 volatile int agachado = 0;
@@ -107,53 +147,50 @@ volatile boolean runn = 0;
 int block_buttons = 0;
 
 void loop() {
-  if (muerto == 0) {
+  if (dead == 0) {
     display.clearDisplay();
 
     ////////dibujar fondo/////////////
-    display.drawBitmap(20, 0, cloud, 32, 24, 1);//el primero es la dcha, el segundo el alto
+    display.drawBitmap(17, 0, cloud, 32, 24, 1);//el primero es la dcha, el segundo el alto
     //display.drawBitmap(36, 0, cloud, 32, 24, 1);
     display.drawBitmap(95, 5, cloud, 32, 24, 1);
-    //display.drawLine(0,63,128,63,1);
-
-    //  display.setTextSize(1); //////////esto seria para abajo en el centro
-    //  display.setCursor(40, 57);
-    //    display.print("lives:");
-    //    display.setCursor(75, 57);//la primera debe ser la del anterior +35
-    //    display.print(lives);
-
-
     ///////////////////////////////////
     ////////dibujar marios/////////////
 
     if (block_buttons == 0) {
       if (digitalRead(11) == 0) {
-        agachado = duracionsalto;                                    //duracion de agacharse
+        agachado = jumpduration;                                    //duracion de agacharse
         block_buttons = 1;
       }
       if (digitalRead(3) == 0) {
-        saltar = duracionsalto;                                      //duracion de salto
+        saltar = jumpduration;                                      //duracion de salto
         block_buttons = 1;
       }
     }
 
     if (saltar > 0 && block_buttons) {
-      if (saltar > (duracionsalto - 1)) {
+      if (saltar > (jumpduration - 1)) {
         display.drawBitmap(0, 30, mario_jump, 16, 28, 1);
+
+        tone(9, jumpsound, 10);
       }
       else if (saltar > 1) {
         display.drawBitmap(0, 18, mario_jump, 16, 28, 1);
+
+        state = 1;
       } else {
         display.drawBitmap(0, 30, mario_jump, 16, 28, 1);
       }
       saltar--;
-      estado = 1;
     } else if (agachado > 0 && block_buttons) {
       display.drawBitmap(0, 44, mario_bend, 19, 20, 1);
+      if (agachado > jumpduration - 1) {
+        tone(9, bendsound, 10);
+      }
       agachado--;
-      estado = 2;
+      state = 2;
     } else {
-      estado = 0;
+      state = 0;
       if (runn == 0) {
         display.drawBitmap(0, 36, mario2, 16, 28, 1);
         runn = 1;
@@ -167,7 +204,7 @@ void loop() {
     ///////////dibujar enemigos/////////////
 
     if (lock == 0) {
-      rando = random(0, 2); //con esto 50% 0 50% 1 xq el ultimo no se alcanza
+      rando = random(0, 2); // 50% 0, 50% 1
       lock = 1;
     }
     if (rando == 0) {
@@ -176,15 +213,15 @@ void loop() {
     } else {
       display.drawBitmap(enemie1_x, 47, enemie, 17, 17, 1);
     }
-    if (enemie1_x <= -30) { //no muy bajo xq cuando la velocidad sea -5 por ejemplo igual no lo alcanza, ni muy alto para que se lleue e aver el cambio en pantalla
+    if (enemie1_x <= -30) { //not so big to be displayed the change, and not so small because is not changing 1 by 1 and could not be raised.
       lock = 0;
       enemie1_x = 200;
     }
-    enemie1_x = enemie1_x + vel_enem;
+    enemie1_x = enemie1_x + enem_vel;
 
     ////////////////segundo enemigo/////////////
     if (lock2 == 0) {
-      rando2 = random(0, 13); //de 0 a 12 CAMBIAR POR 12 AL ACABAR
+      rando2 = random(0, 13); //from 0 to 12
       lock2 = 1;
     }
     if (rando2 > 9) {
@@ -195,92 +232,103 @@ void loop() {
     else {
       display.drawBitmap(enemie2_x, 47, enemie, 17, 17, 1);
     }
-    if (enemie2_x <= -30) { //no muy bajo xq cuando la velocidad sea -5 por ejemplo igual no lo alcanza, ni muy alto para que se lleue e aver el cambio en pantalla
+    if (enemie2_x <= -30) {
       lock2 = 0;
       enemie2_x = 200;
     }
-    enemie2_x = enemie2_x + vel_enem;
+    enemie2_x = enemie2_x + enem_vel;
 
-    ////////////aumentar velocida enemigos con el tiempo/////////////
-    tiempo_actual = millis();
-    if (((tiempo_actual - tiempo_aux) > 30000)) { // Esta parte se ejecuta cada
-      tiempo_aux = tiempo_actual;             // 35000ms es decir cada 50 segundos
-      vel_enem--;
-      duracionsalto--;//negativo xq vamos en direccion -x y asi se aumenta
+    ////////////increase speed over time/////////////
+    current_time = millis();
+    if (((current_time - aux_time) > 30000)) { // next lines happens every 30000ms = 30 seconds
+      aux_time = current_time;
+      enem_vel--;
+      jumpduration--;//negative bc we are going in the direction -x so is not raising
     }
 
-    ///////////comprobar colision/////////////
-    if ((enemie2_x <= 12) && (enemie2_x > -23) && locklives == 0) { //con -22 con el misil va muy bien con el enemie con 25
-      if (((estado == 2 || estado == 0) && rando2 > 9)) {
-        lives++;
-        locklives = 1; //bloqueo para que no se quiten muchas vidas de golpe
-
+    ///////////check collision/////////////
+    if ((enemie2_x < 10) && (enemie2_x > -23) && locklives == 0) { //at -23 is out of the screen
+      if (((state == 2 || state == 0) && rando2 > 9)) {
+        if (lives < 5) {
+          lives++;
+          locklives = 1; //blocked to add just one live
+          tone(9, crashsound2, 100);
+          delay(100);
+          tone(9, crashsound1, 300);
+        }
       } else if (lockscore == 0) {
         score++;
         lockscore = 1;
       }
     }
-    if ((enemie1_x <= 12) && (enemie1_x > -23) && locklives == 0) { //con -22 con el misil va muy bien con el enemie con 25
-      if (((estado == 2 || estado == 0) && rando == 1) || ((estado == 1 || estado == 0) && rando == 0)) {
+    if ((enemie1_x < 10) && (enemie1_x > -23) && locklives == 0) {
+      if (((state == 2 || state == 0) && rando == 1) || ((state == 1 || state == 0) && rando == 0)) {
         lives--;
         locklives = 1;
+        tone(9, crashsound1, 100);
+        delay(100);
+        tone(9, crashsound2, 300);
       } else if (lockscore == 0) {
         score++;
         lockscore = 1;
       }
     }
-    if ((enemie2_x <= 12) && (enemie2_x > -23) && locklives == 0) {
-      if (((estado == 2 || estado == 0) && rando2 > 4 && rando2 < 10) || ((estado == 1 || estado == 0) && rando2 < 5)) {
+    if ((enemie2_x < 10) && (enemie2_x > -23) && locklives == 0) {
+      if (((state == 2 || state == 0) && rando2 > 4 && rando2 < 10) || ((state == 1 || state == 0) && rando2 < 5)) {
         lives--;
         locklives = 1;
+        tone(9, crashsound1, 100);
+        delay(100);
+        tone(9, crashsound2, 300);
       } else if (lockscore == 0) {
         score++;
         lockscore = 1;
       }
     }
 
-    if (enemie1_x < -20 || enemie2_x < -20) {//desbloqueamos
+    if (enemie1_x < -20 || enemie2_x < -20) {//unlock
       locklives = 0;
       lockscore = 0;
     }
 
-
-    if (lives == 0) {//cambiar por musiquita super mario
-      //tone(9, 200, 300);
-      //delay(300);
-      //tone(9, 250, 200);
-      //delay(200);
-      //tone(9, 300, 300);
-      //delay(300);
-      muerto = 1;
-    }
     display.setTextSize(1);
     display.setCursor(50, 0);
     display.print("L:");
-    display.setCursor(63, 0);//la primera debe ser la del anterior +35
+    display.setCursor(63, 0);
     display.print(lives);
     display.setCursor(75, 0);
     display.print("S:");
-    display.setCursor(88, 0);//la primera debe ser la del anterior +35
+    display.setCursor(88, 0);
     display.print(score);
     display.display();
+
+
+    if (lives == 0) {
+      delay(500);
+      sing(1);
+      dead = 1;
+    }
   }
-  if (muerto == 1) {
-    display.setTextSize(2);
-    display.setFont();
-    display.setTextColor(WHITE);
-    display.setCursor(5, 20);
+  if (dead == 1) {
+    display.clearDisplay();
+    display.setTextSize(0);
+    display.setFont(&FreeSans9pt7b);
+    display.setCursor(7, 25);
     display.println("GAME OVER");
+
+    display.setFont();
+    display.setCursor(37, 36);
+    display.print("Score: ");
+    display.setCursor(73, 36);
+    display.print(score);
+
+    display.setCursor(0, 54);
+    display.print("Press C to play again");
+
+
     display.display();
-    delay(2000);
-    if (digitalRead(3) == 0)
+    if (digitalRead(12) == 0)
     {
-      //      tone(9, 280, 300);
-      //      delay(300);
-      //      tone(9, 250, 200);
-      //      delay(200);
-      //      tone(9, 370, 300);
-      //      delay(300);
       reinicio();
     }
   }
@@ -290,31 +338,55 @@ void loop() {
 
 void reinicio()
 {
-  muerto = 0;
-  tiempo_actual = 0;
+  dead = 0;
+  current_time = 0;
   lives = 1;
   score = 0;
   enemie1_x = 130;
   enemie2_x = 240;
-  vel_enem = -5;
-  tiempo_actual = 0;
-  tiempo_aux = 0;
+  enem_vel = -5;
+  current_time = 0;
+  aux_time = 0;
   lock = 0;
   lock2 = 0;
   block_buttons = 0;
-  duracionsalto = 11;
+  jumpduration = 11;
   locklives = 0;
   rando = 0;
   rando2 = 0;
-  estado = 0;
+  state = 0;
 }
 
-void beep(int note, int duration)
-{
-  //Play tone on buzzerPin
-  tone(9, note, duration);
-  delay(duration);
-  noTone(9);
-  delay(50);
+void sing(int song) {
+  if (song == 0) {
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1000 / tempo[thisNote];
+      buzz(melodyPin, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      buzz(melodyPin, 0, noteDuration);
+    }
+  } else if (song == 1) {
+    int size = 13;
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1000 / tempo[thisNote];
+      buzz(melodyPin, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      buzz(melodyPin, 0, noteDuration);
+    }
+  }
+}
 
+void buzz(int targetPin, long frequency, long length) {
+  digitalWrite(13, HIGH);
+  long delayValue = 1000000 / frequency / 2;
+  long numCycles = frequency * length / 1000;
+  for (long i = 0; i < numCycles; i++) {
+    digitalWrite(targetPin, HIGH);
+    delayMicroseconds(delayValue);
+    digitalWrite(targetPin, LOW);
+    delayMicroseconds(delayValue);
+  }
 }
